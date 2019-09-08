22f8a458ecbd80d509343a0ac441bc9dfc41ca5f
Kalogiannis Xristos <xristos_kalogiannis@yahoo.com>
2019-09-08 12:04:27 +0300

    ATM Project

---
+++ src/atm/ATM.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author xristos
+ */
+public class ATM extends Option{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws  IOException {
+       
+        
+         Option menu = new Option();
+         
+    }
+
+    }
+    
+
